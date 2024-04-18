c867ff357af7ad2e83a5bb78a3af067304350016
Aurélien <aurelien.strimelle@bstorm.be>
2024-04-18 10:48:58 +0200

    Add Pokemon in Team 🎳

--- src/containers/pokemon/pokemon-show.jsx
+++ src/containers/pokemon/pokemon-show.jsx
@@ -1,5 +1,7 @@
 import { useSelector } from 'react-redux';
 import PokemonDetail from '../../components/pokemon/pokemon-detail.jsx';
+import { useDispatch } from 'react-redux';
+import { addPokemonTeam } from '../../store/pokemon/pokemon.action.js';
 
 const PokemonShow = () => {
 
@@ -9,12 +11,21 @@ const PokemonShow = () => {
         error
     } = useSelector(state => state.pokemon.search);
 
+    const dispatch = useDispatch();
+
+    const handleAddPokemonTeam = () => {
+        dispatch(addPokemonTeam(pokemon));
+    }
+
     return (
         <div>
             {isLoading ? (
                 <p>Recherche du pokemon en cours...</p>
             ) : pokemon ? (
-                <PokemonDetail {...pokemon} />
+                <>
+                    <button onClick={handleAddPokemonTeam}>Ajouter à mon équipe</button>
+                    <PokemonDetail {...pokemon} />
+                </>
             ) : error ? (
                 <>
                     <p>Error lors de la recherche 😥</p>
